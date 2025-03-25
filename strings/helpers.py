12208efb5c74fcb83060b5d4513b6de9b956d6a1
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-25 15:27:33 +0300

    Update helpers.py

--- strings/helpers.py
+++ strings/helpers.py
@@ -3,7 +3,7 @@
 #
 # This file is part of < https://github.com/Forkmusic/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/isagzbyk46/Forkmusic/blob/master/Licence >
+# Please see < https://github.com/isagzbyk46/Forkmusic/blob/master/LİCENSE >
 #
 # All rights reserved.
 
